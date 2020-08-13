e0ac6fcb8fa8b51fd4dd492ec839cc341ef8ebc2
erlan artykbaev <erik.2405@mail.ru>
2020-08-14 05:50:19 +0600

    react router installed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import './App.css';
 import Header from './components/Header'
 import Sidebar from './components/Sidebar';
 import Recomended from './components/Recomended';
+import {Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+			<Router>
+				<Switch>
+
+					<Route to='/'>
+						<div className='app__page'>
+							<Sidebar />
+							<Recomended />
+						</div>	
+					</Route>
+
+					<Route to='/search'>
+						<div className='app__page'>
+							<Sidebar />
+						</div>	
+					</Route>
+
+				</Switch>
+			</Router>
 			<Header />
-			<div className='app__page'>
-				<Sidebar />
-				<Recomended />
-			</div>
     </div>
   );
 }
