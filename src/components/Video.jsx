296c8b7a2fb58862d7fb0f107bf4d31268cea589
erlan artykbaev <erik.2405@mail.ru>
2020-08-29 22:35:55 +0600

    video scss is ready

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import {Avatar} from '@material-ui/core'
 
-import './css/Video.css'
 
 const Video = (props) => {
 	return(
 		<div className='video'> 
-			<img className='video__thumbnail' src={props.img} alt={props.title} />
-			<div className='video__info'>
-				<Avatar className='video__avatar' alt={props.channel} src={props.channelImg} />
-				<div className='video__text'>
+			<img className='thumbnail' src={props.img} alt={props.title} />
+			<div className='info'>
+				<Avatar className='avatar' alt={props.channel} src={props.channelImg} />
+				<div className='text'>
 					<h4>{props.title}</h4>
 					<p>{props.channel}</p>
 					<p>pub: {props.timestamp}</p>
