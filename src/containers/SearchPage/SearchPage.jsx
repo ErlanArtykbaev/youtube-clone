22da19f07ac42afe700d6a4ab7790a075ca8ee82
erlan artykbaev <erik.2405@mail.ru>
2020-08-29 22:41:42 +0600

    searchpage scss is ready

--- src/containers/SearchPage/SearchPage.jsx
+++ src/containers/SearchPage/SearchPage.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 
-import './SearchPage.css'
-
 import TuneRoundedIcon from '@material-ui/icons/TuneRounded'
 
 const SearchPage = (props) => {
 	return(
 		<div className='searchPage'>
-			<div className='searchPage__filter'>
+			<div className='filter'>
 				<TuneRoundedIcon />
 				<h2>Filter</h2>
 			</div>
-			<div className='searchPage__videos'>{props.videos}</div>
+			<div className='videos'>{props.videos}</div>
 		</div>
 	)
 }
